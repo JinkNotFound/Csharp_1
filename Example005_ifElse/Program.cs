183317a474a8d1b15d7dcb2c828cede2d556c6a3
Pavel <jinkofficially@gmail.com>
2023-10-04 18:41:47 +0300

    if else

---
+++ Example005_ifElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username  = Console.ReadLine();
+
+if(username.ToLower() == "Pavel")
+{
+    Console.WriteLine("Урааа пятница");
+}
+else
+{
+    Console.Write("Првиет, ");
+    Console.WriteLine(username);
+}
